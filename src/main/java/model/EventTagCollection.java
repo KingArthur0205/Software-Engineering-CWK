2f07d76365ba7bf1607306911bb3b76baa7b145f
Guanzhong Pan <gpan20020205@gmail.com>
2023-03-21 20:12:17 +0000

    Add the new Model class EventTagCollection. This class now has only one

---
+++ src/main/java/model/EventTagCollection.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * {@link EventTagCollection} holds a map of names and values, corresponding each to an {@link Event}’s tag name and the
+ * value selected for it.
+ */
+public class EventTagCollection {
+    private Map<String, String> tags;
+
+    /**
+     * Create a new EventTagCollection with an empty map of Tags.
+     */
+    public EventTagCollection() {
+        this.tags = new HashMap<String, String>();
+    }
+
+    /**
+     * Create a new EventTagCollection with the String valuesOfEachTag
+     * @param valuesOfEachTag  The String has the form "name1=value2,name2=value2" and etc. and will be parsed to obtain
+     * the map of tags with their corresponding Tags.
+     */
+    public EventTagCollection(String valuesOfEachTag) {
+        String[] pairs = valuesOfEachTag.split(",");
+        for (String pair : pairs) {
+            String[] tagAndValue = pair.split("=");
+            String tagName = tagAndValue[0];
+            String value = tagAndValue[1];
+            tags.put(tagName, value);
+        }
+    }
+}

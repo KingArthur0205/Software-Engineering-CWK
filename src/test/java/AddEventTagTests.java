a8dd81e3cd4a889118818927bd36be1d0e6a1633
Guanzhong Pan <gpan20020205@gmail.com>
2023-03-22 10:20:22 +0000

    Added the AddEventTagTests class The class currently only has a createEventTag function that creates a tag using the AddEventTagCommand

--- src/test/java/AddEventTagTests.java
+++ src/test/java/AddEventTagTests.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class AddEventTagTests {
+import command.AddEventTagCommand;
+import controller.Controller;
+import model.EventTag;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+public class AddEventTagTests extends ConsoleTest{
+    private static EventTag createEventTag(Controller controller, String tagName, Set<String> possibleValues,
+                                           String defaultValue) {
+        AddEventTagCommand eventCmd = new AddEventTagCommand(tagName, possibleValues, defaultValue);
+        controller.runCommand(eventCmd);
+        return eventCmd.getResult();
+    }
 }

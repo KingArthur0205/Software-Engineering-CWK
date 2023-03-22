d0384c465cfe352ca97ebf57e27780806b91682e
Guanzhong Pan <gpan20020205@gmail.com>
2023-03-22 11:01:18 +0000

    Added the first test to test Add Tag when User not Logged in

--- src/test/java/AddEventTagTests.java
+++ src/test/java/AddEventTagTests.java
@@ -3,6 +3,8 @@ import controller.Controller;
 import model.EventTag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class AddEventTagTests extends ConsoleTest{
@@ -11,5 +13,13 @@ public class AddEventTagTests extends ConsoleTest{
         AddEventTagCommand eventCmd = new AddEventTagCommand(tagName, possibleValues, defaultValue);
         controller.runCommand(eventCmd);
         return eventCmd.getResult();
+    }
+
+    @Test
+    void addEventTagNotLoggedIn() {
+        Controller controller = createController();
+        startOutputCapture();
+        createEventTag(controller, "tag1", new HashSet<>(Arrays.asList("value1")), "defValue");
+        stopOutputCaptureAndCompare("ADD_EVENT_TAG_USER_NOT_STAFF");
     }
 }

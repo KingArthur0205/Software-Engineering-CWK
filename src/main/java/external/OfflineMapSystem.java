47138a2c497067ba4c838021a94cf329851d02be
KingArthur0205 <gpan20020205@gmail.com>
2023-03-29 15:21:46 +0100

    Implement MapSystem

--- src/main/java/external/OfflineMapSystem.java
+++ src/main/java/external/OfflineMapSystem.java
@@ -1,34 +1,71 @@
 package external;
 
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.ResponsePath;
+import com.graphhopper.config.CHProfile;
+import com.graphhopper.config.Profile;
 import com.graphhopper.util.Translation;
+import com.graphhopper.util.TranslationMap;
 import com.graphhopper.util.shapes.GHPoint;
 import model.TransportMode;
+
+import java.util.Locale;
 
 public class OfflineMapSystem implements MapSystem{
     private GraphHopper hopper;
     public OfflineMapSystem() {
         hopper = new GraphHopper();
+        hopper.setOSMFile("scotland-latest.osm.pbf");
+
+        hopper.setGraphHopperLocation("target/routing-graph-cache");
+        hopper.setProfiles(new Profile("foot").setVehicle("foot"), new Profile("car").setVehicle("car"),
+                new Profile("wheelchair").setVehicle("wheelchair"), new Profile("bike").setVehicle("bike"));
+
+        // Setup the map data
+        hopper.importOrLoad();
     }
 
     @Override
     public GHPoint convertToCoordinates(String address) {
-        return null;
+        return GHPoint.fromString(address);
     }
 
     @Override
     public boolean isPointWithinMapBounds(GHPoint addressPoint) {
-        return false;
+        return hopper.getBaseGraph().getBounds().contains(addressPoint.getLat(), addressPoint.getLon());
     }
 
     @Override
     public ResponsePath routeBetweenPoints(TransportMode transportMode, GHPoint startAddress, GHPoint destinationAddress) {
-        return null;
+        String transMode = "";
+        switch (transportMode) {
+            case car -> transMode = "car";
+            case bike -> transMode = "bike";
+            case foot -> transMode = "foot";
+            case wheelchair -> transMode = "wheelchair";
+            default -> transMode = "foot";
+        }
+
+        GHRequest req = new GHRequest(startAddress, destinationAddress).setProfile(transMode).setLocale(Locale.UK);
+        GHResponse rsp = hopper.route(req);
+        return rsp.getBest();
     }
 
     @Override
     public Translation getTranslation() {
-        return null;
+        return hopper.getTranslationMap().getWithFallBack(Locale.UK);
+    }
+
+    /**
+     * Properly shutdown {@link GraphHopper}.
+     */
+    public void close() {
+        hopper.close();
+    }
+
+    public GraphHopper getHopper() {
+        return hopper;
     }
 }

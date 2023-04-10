fd4eb4d3100f94b0f0561d5e67b1421b3fd293e6
KingArthur0205 <gpan20020205@gmail.com>
2023-04-10 11:04:07 +0100

    Added JavaDoc for AddEventTagCommand

--- src/main/java/command/AddEventTagCommand.java
+++ src/main/java/command/AddEventTagCommand.java
@@ -33,10 +33,15 @@ public class AddEventTagCommand implements ICommand<EventTag>{
         this.defaultValue = defaultValue;
     }
 
+    /**
+     * @param context object that provides access to global application state
+     * @param view    allows passing information to the user interface
+     * @verifies.that
+     */
     @Override
     public void execute(Context context, IView view) {
         User currentUser = context.getUserState().getCurrentUser();
-        // Check if user is Staff
+        // Verify if current user is a Staff
         if (!(currentUser instanceof Staff)) {
             view.displayFailure("AddEventTagCommand",
                     LogStatus.ADD_EVENT_TAG_USER_NOT_STAFF,
@@ -46,7 +51,7 @@ public class AddEventTagCommand implements ICommand<EventTag>{
             return;
         }
 
-        // Check the tagName doesn't clash with any existing ones
+        // Verify that the tagName doesn't clash with any existing tag names
         IEventState eventState = context.getEventState();
         if (eventState.getPossibleTags().containsKey(tagName)) {
             view.displayFailure("AddEventTagCommand", LogStatus.ADD_EVENT_TAG_NAME_CLASH,
@@ -55,7 +60,7 @@ public class AddEventTagCommand implements ICommand<EventTag>{
             return;
         }
 
-        // Check if there are at least two tag values
+        // Verify that at least two tag values are provided
         if (tagValues == null || tagValues.size() < 2) {
             view.displayFailure("AddEventTagCommand", LogStatus.ADD_EVENT_TAG_TOO_FEW_POSSIBLE_VALUES,
                     Map.of("tagValues", tagValues));
@@ -63,7 +68,7 @@ public class AddEventTagCommand implements ICommand<EventTag>{
             return;
         }
 
-        // Check if the default value is in the list of possible values
+        // Verify that the default value is in the list of possible values
         if (!(tagValues.contains(defaultValue))) {
             view.displayFailure("AddEventTagCommand", LogStatus.ADD_EVENT_TAG_DEFAULT_VALUE_NOT_POSSIBLE,
                     Map.of("tagValues", tagValues, "defaultValues", defaultValue));
@@ -71,7 +76,7 @@ public class AddEventTagCommand implements ICommand<EventTag>{
             return;
         }
 
-        // Add tag
+        // Add tag to the system
         EventTag tag = eventState.createEventTag(tagName, tagValues, defaultValue);
         view.displaySuccess("AddEventTagCommand", LogStatus.ADD_EVENT_TAG_SUCCESS,
                 Map.of("tagName", tagName, "tagValues", tagValues, "defaultValue", defaultValue));
@@ -79,7 +84,7 @@ public class AddEventTagCommand implements ICommand<EventTag>{
     }
 
     /**
-     * @return tag corresponding to the newly created Tag if successful and null otherwise
+     * @return tag corresponding to the newly created {@link EventTag} if successful and null otherwise
      */
     @Override
     public EventTag getResult() {
--- src/main/java/model/EventTagCollection.java
+++ src/main/java/model/EventTagCollection.java
@@ -21,15 +21,20 @@ public class EventTagCollection {
 
     /**
      * Create a new EventTagCollection with the String valuesOfEachTag
-     * @param valuesOfEachTag  The String has the form "name1=value2,name2=value2" and etc. and will be parsed to obtain
+     * @param valuesOfEachTag  The String has the form "name1=value2,name2=value2...". and will be parsed to obtain
      * the map of tags with their corresponding Tags.
      */
     public EventTagCollection(String valuesOfEachTag) {
         this();
         if (valuesOfEachTag != null) {
             String[] pairs = valuesOfEachTag.split(",");
+
+            // Split each "name=value" pair to two individual String of name and value and store them in tags.
             for (String pair : pairs) {
                 String[] tagAndValue = pair.split("=");
+                // Verify that each pair is parsed into separate name and value
+                assert(tagAndValue.length == 2);
+
                 String tagName = tagAndValue[0];
                 String value = tagAndValue[1];
                 tags.put(tagName, value);
@@ -37,6 +42,9 @@ public class EventTagCollection {
         }
     }
 
+    /**
+     * @return a map of {@link EventTag}s and selected valeus for each of tag
+     */
     public Map<String, String> getTags() {
         return tags;
     }
